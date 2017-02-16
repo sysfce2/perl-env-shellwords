d6f611f3c31c4fc814c9b981c4ad998e92e0193c
Graham Ollis <plicease@cpan.org>
2017-02-16 19:54:23 +1100

    original implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Env::ShellWords [![Build Status](https://secure.travis-ci.org/plicease/Env-ShellWords.png)](http://travis-ci.org/plicease/Env-ShellWords)
+
+Environment variables for arguments as array
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Env/ShellWords.pm
+++ lib/Env/ShellWords.pm
@@ -2,8 +2,121 @@ package Env::ShellWords;
 
 use strict;
 use warnings;
+use Text::ParseWords qw( shellwords );
 
 # ABSTRACT: Environment variables for arguments as array
 # VERSION
+
+sub TIEARRAY
+{
+  my($class, $name) = @_;
+  bless \$name, $class;
+}
+
+sub FETCH
+{
+  my($self, $key) = @_;
+  my @list = shellwords($ENV{$$self});
+  $list[$key];
+}
+
+sub _render
+{
+  my($self, @list) = @_;
+  $ENV{$$self} = join ' ', map {
+    my $value = $_;
+    $value = '' unless defined $value;
+    $value =~ s/(\s)/\\$1/g;
+    $value eq '' ? "''" : $value;
+  } @list;
+}
+
+sub STORE
+{
+  my($self, $key, $value) = @_;
+  my @list = shellwords($ENV{$$self});
+  $list[$key] = $value;
+  _render($self, @list);
+  $value;
+}
+
+sub FETCHSIZE
+{
+  my($self) = @_;
+  my @list = shellwords($ENV{$$self});
+  $#list + 1;
+}
+
+sub STORESIZE
+{
+  my($self, $count) = @_;
+  my @list = shellwords($ENV{$$self});
+  $#list = $count - 1;
+  _render($self, @list);
+  return;
+}
+
+sub CLEAR
+{
+  my($self) = @_;
+  _render($self);
+  return;
+}
+
+sub PUSH
+{
+  my($self, @values) = @_;
+  _render($self, shellwords($ENV{$$self}), @values);
+  return;
+}
+
+sub POP
+{
+  my($self) = @_;
+  my @list = shellwords($ENV{$$self});
+  my $value = pop @list;
+  _render($self, @list);
+  return $value;
+}
+
+sub SHIFT
+{
+  my($self) = @_;
+  my($value, @list) = shellwords($ENV{$$self});
+  _render($self, @list);
+  return $value;
+}
+
+sub UNSHIFT
+{
+  my($self, @values) = @_;
+  _render($self, @values, shellwords($ENV{$$self}));
+  return;
+}
+
+sub SPLICE
+{
+  my($self, $offset, $length, @values) = @_;
+  my @list = shellwords($ENV{$$self});
+  my @ret = splice @list, $offset, $length, @values;
+  _render($self, @list);
+  @ret;
+}
+
+sub DELETE
+{
+  my($self, $key) = @_;
+  my @list = shellwords($ENV{$$self});
+  my $value = delete $list[$key];
+  _render($self, @list);
+  return $value;
+}
+
+sub EXISTS
+{
+  my($self, $key) = @_;
+  my @list = shellwords($ENV{$$self});
+  return exists $list[$key];
+}
 
 1;
